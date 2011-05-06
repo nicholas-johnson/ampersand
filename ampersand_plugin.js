2316bf60125f9b70e0c5d5c11a286f5e36fbb175
Nic <nicholas@wildinvention.co.uk>
2011-05-06 22:18:45 +0100

    README.md
    Ampersand will now only replace ampersands in text nodes. I may have introduced an IE bug. Will need to wait till Monday to check...

--- ampersand_plugin.js
+++ ampersand_plugin.js
@@ -1,7 +1,24 @@
 (function($){
-  $.fn.ampersand = function() {
+  $.fn.ampersand = function(ampClass) {
+    if (ampClass === undefined) {
+      ampClass = "ampersand";
+    }
+    var get_contents = function(el) {
+      $(el).contents().each(function(i,el) {
+        if (el.nodeType === 3) {
+          if (el.textContent) {
+            el.textContent = el.textContent.replace(/&/g, "#ampersandmarker#")
+          } else if (el.text) /* IE */ {
+            el.text = el.text.replace(/&/g, "#ampersandmarker#")
+          }
+        } else if (el.nodeType === 1) {
+          get_contents(el);
+        }
+      });
+    }
     $(this).each(function(i, el) {
-      el.innerHTML = el.innerHTML.replace(/&amp;/g, "<span class='ampersand'>&amp;</span>")
+      get_contents(el); /* Walk the dom finding only ampersands in text nodes. Don't bother splitting them. */
+      el.innerHTML = el.innerHTML.replace(/#ampersandmarker#/g, "<span class='"+ampClass+"'>&amp;</span>") /* replace the markers. */
     });
     return this;
   };
